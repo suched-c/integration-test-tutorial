99b32d8c2d3a290c449d441cc93ad73c23b2723d
suched.chai <suched.chai@kkpfg.com>
2021-09-23 18:31:46 +0700

    just commit

---
+++ ios/RunnerTests/RunnerTests.m
@@ -0,0 +1,11 @@
+//
+//  RunnerTests.m
+//  RunnerTests
+//
+//  Created by Suched Chaisiricharoenkul on 23/9/2564 BE.
+//
+
+#import <XCTest/XCTest.h>
+@import integration_test;
+
+INTEGRATION_TEST_IOS_RUNNER(RunnerTests)
